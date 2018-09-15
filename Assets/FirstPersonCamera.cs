dbc40b7af9bc1e0b5b6c1b6fc566043ca888d1b2
Jack Youstra <jack@youstra.com>
2018-09-15 15:46:18 -0500

    Added unity lady and did some camera work

---
+++ Assets/FirstPersonCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour {
+    public GameObject player;
+    public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = player.transform.localPosition + offset;
+        transform.localRotation = player.transform.localRotation;
+	}
+}
--- Assets/Yandere/Player.cs
+++ Assets/Yandere/Player.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	private CharacterController controller;
+    private Animator animator;
     private float xRotation;
     public float Speed;
+    public float FowardMultiplier = 2.0f;
     public float mouseXSensitivity;
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(transform.localRotation * move * Time.deltaTime * -1.0f * Speed);
+
+    // Update is called once per frame
+    void Update() {
+        float speed;
+        speed = Vector3.Dot(controller.velocity, transform.localRotation * Vector3.forward);
+        animator.SetFloat("Speed", speed);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (move.z > 0) {
+            move.Scale(new Vector3(1.0f, 1.0f, FowardMultiplier));
+        }
+        controller.Move(transform.localRotation * move * Time.deltaTime * Speed);
         xRotation += Input.GetAxis("Mouse X") * mouseXSensitivity * Time.deltaTime;
         if (xRotation < -360.0f) {
             xRotation += 360.0f;
